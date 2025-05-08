ddd7b6560eacfe568a97b308ae6cb04b67360663
Krethik128 <krethik3114@gmail.com>
2025-05-08 23:58:09 +0530

    Practised JUnit

---
+++ src/test/java/ListManagerTest.java
@@ -0,0 +1,31 @@
+import org.example.ListManager;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ListManagerTest {
+    @Test
+    void testAddElement() {
+        List<Integer> list = new ArrayList<>();
+        ListManager.addElement(list, 10);
+        assertTrue(list.contains(10));
+    }
+    @Test
+    void testRemoveElement() {
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        ListManager.removeElement(list, 10);
+        assertFalse(list.contains(10));
+    }
+
+    @Test
+    void testGetSize() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        assertEquals(2, ListManager.getSize(list));
+    }
+}
---
+++ src/test/java/StringUtilsTest.java
@@ -0,0 +1,21 @@
+import org.example.StringUtils;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringUtilsTest {
+    @Test
+    public void testReverse() {
+        assertEquals("cba", StringUtils.reverse("abc"));
+    }
+
+    @Test
+    public void testIsPalindrome(){
+        assertTrue(StringUtils.isPalindrome("madam"));
+        assertFalse(StringUtils.isPalindrome("hello"));
+    }
+    @Test
+    void testToUpperCase() {
+        assertEquals("HELLO", StringUtils.toUpperCase("hello"));
+    }
+}
